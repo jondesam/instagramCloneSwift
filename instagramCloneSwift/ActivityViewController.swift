1d3e25a12d631d5ce891a47a2ac1bd9daf0800db
Jonghyun <godaiys@hotmail.com>
2019-04-29 18:00:42 -0400

    first commit and built up tp tab bar controller

---
+++ instagramCloneSwift/ActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityViewController.swift
+//  instagramCloneSwift
+//
+//  Created by MyMac on 2019-04-29.
+//  Copyright © 2019 Apex. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ instagramCloneSwift/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  instagramCloneSwift
+//
+//  Created by MyMac on 2019-04-29.
+//  Copyright © 2019 Apex. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
